3f00805564ba5de7e7c4dd98d7c54f0b1cd12ed2
Nehemiah <NehemiahAK354@outlook.com>
2019-04-14 18:04:44 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,27 @@
+/* 
+* MIT License
+
+* Copyright (c) 2019 Nehemiah
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 var trainingData = []; /* Put data in this array in the form of numbers. */
 
 var cnt = trainingData.length; /* Find the amount of mumbers in the trainingData array. */
@@ -29,7 +53,7 @@ var toleranceLimit = null; /* Put the tolerance limit here. */
 
 function decision() {
 
-if (Math.abs(avgTwo - avg) < toleranceLimit) {
+if (Math.abs(avgTwo - avg) <= toleranceLimit) {
 return true;
 } 
 
